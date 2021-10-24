baf1840ffc16f985333cf76b4c2debd99e8b7a7a
cabucabu <B-c-s_LP.00@hotmail.com>
2021-10-25 02:22:08 +0700

    ok2

--- lib/question.dart
+++ lib/question.dart
@@ -12,7 +12,7 @@ class Question extends StatelessWidget {
       margin: EdgeInsets.all(12),
       child: Text(
         questionText,
-        style: TextStyle(fontSize: 28),
+        style: TextStyle(fontSize: 30),
         textAlign: TextAlign.center,
       ),
     );
