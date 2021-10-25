68e757b709e236f0af65835db61b6c39ccae03a9
cabucabu <B-c-s_LP.00@hotmail.com>
2021-10-25 14:05:26 +0700

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/result.dart';
 import './question.dart';
 import './answer.dart';
+import './quiz.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,47 +15,64 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 5},
+        {'text': 'Red', 'score': 3},
+        {'text': 'Green', 'score': 4},
+        {'text': 'White', 'score': 5},
+      ]
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 1},
+        {'text': 'Snake', 'score': 2},
+        {'text': 'Elephant', 'score': 3},
+        {'text': 'Lion', 'score': 4},
+      ]
+    },
+    {
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': [
+        {'text': 'max1', 'score': 1},
+        {'text': 'max2', 'score': 2},
+        {'text': 'max3', 'score': 3},
+        {'text': 'max4', 'score': 4},
+      ],
+    },
+  ];
+
   var _questionIndex = 0;
 
   void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
+    if (_questionIndex < _questions.length)
+      print('We have more question!');
+    else
+      print('No more question!');
     print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    var question = [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': ['Black', 'Red', 'Green', 'White']
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion']
-      },
-      {
-        'questionText': 'Who\'s your favorite instructor?',
-        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion']
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('My Firth App'),
-          ),
-          body: Column(
-            children: [
-              Question(
-                question[_questionIndex]['questionText'] as String,
-              ),
-              ...(question[_questionIndex]['answers'] as List<String>)
-                  .map((answer) {
-                return Answer(_answerQuestion, answer);
-              }).toList()
-            ],
-          )),
+        appBar: AppBar(
+          title: Text('My Firth App'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(),
+      ),
     );
   }
 }
